1c078002c176bab5f51efe09c56e0b1c731eb34c
@SarxRom <2233073251@correo.ler.uam.mx>
2025-07-21 16:28:54 -0600

    ahhh

--- camaraweb/sketch.js
+++ camaraweb/sketch.js
@@ -1,7 +1,14 @@
+let capture;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+ capture= createCapture(VIDEO,{flipped: true});
+ capture.size(200,200);
+ capture.hide(400,400);
+
 }
 
 function draw() {
   background(220);
+  image(capture,420,125,400,300);
 }
